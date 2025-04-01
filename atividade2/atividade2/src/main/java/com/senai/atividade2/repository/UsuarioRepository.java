7b5e09e96a0c9e8ad022baeaf0f3eef7c26932d3
Rafael Vitor <160610194+rafassantos7@users.noreply.github.com>
2025-04-01 10:10:54 -0300

    Delete atividade2/atividade2 directory

--- atividade2/atividade2/src/main/java/com/senai/atividade2/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senai.atividade2.repository;
-
-import com.senai.atividade2.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
-}
